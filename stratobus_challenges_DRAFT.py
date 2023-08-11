cc03cf3fb174566473e5e7a200be2b94a2941c07
reabt <75687966+reabt@users.noreply.github.com>
2023-08-11 12:29:44 +0100

    some refactoring - split up into smaller functions again, working on returning instead of printing data. Still some work needed, and need to confirm whether we can use print statements in pygame

--- stratobus_challenges_DRAFT.py
+++ stratobus_challenges_DRAFT.py
@@ -23,27 +23,30 @@ class Asteroids(Challenge):
         print("fail")
         # body
 
+# the below needs work - currently outputs should-be-readable data as a list, may need to rework into a string with \n's
+# also can't work out how to output this to console??
+    def display_asteroid_data(self, asteroid_output):
+        print(asteroid_output)  # can we print here? This doesn't actually display the data to the player
 
-    def player_enter_asteroid_distance(self, asteroid_distances):
+    def player_enter_asteroid_distance(self, asteroid_distances, asteroid_output):
+        self.display_asteroid_data(asteroid_output) # call this fn to output the data as a return
         attempts = 3
         while attempts > 0:
             inp = input(f"For any of the 3 asteroids that passed near Earth today, enter the miss distance rounded to the nearest km. You have {attempts} attempts remaining... ")
             if inp.isnumeric():
                 if int(inp) in asteroid_distances:
-                    self.success() #call Success function from above if player inputs correct number
+                    self.success()  # call Success function from above if player inputs correct number
                     break
                 else:
                     attempts -= 1
-                    print(f"Incorrect data - try again. {attempts} attempts remaining... ")#add attempt number here too using interpolation?
+                    print(f"Incorrect data - try again. {attempts} attempts remaining... ")
             else:
                 attempts -= 1
-                print(f"Oops, it looks like you entered something that isn't a number! {attempts} attempts remaining... ")#move this so that
+                print(f"Oops, it looks like you entered something that isn't a number! {attempts} attempts remaining... ")  # move this?
         if attempts == 0:
             self.fail()
 
-
-    def get_3_asteroid_data(self):
-        asteroid_distances = []
+    def get_all_asteroid_data(self):
         today_date = date.today()
         today_date_string = str(today_date)
 
@@ -52,33 +55,37 @@ class Asteroids(Challenge):
 
         response = requests.get(short_url)
         data = response.json()
+
+        self.get_3_asteroid_data(data=data, today_date_string=today_date_string)
+
+    def get_3_asteroid_data(self, data, today_date_string):
+        asteroid_distances = []
+        asteroid_output = []
+
         try:
-            first_asteroid_miss_distance_raw = float(data['near_earth_objects'][today_date_string][0]['close_approach_data'][0]['miss_distance']["kilometers"])
-            first_asteroid_miss_distance_rounded = round(first_asteroid_miss_distance_raw, 2)
-            print(f"First asteroid miss distance (km) = {first_asteroid_miss_distance_rounded}")
-            asteroid_distances.append(int(first_asteroid_miss_distance_raw))
-        except:
-            return "No asteroids passed near the Earth today."
+            asteroid_miss_distance_raw = float(data['near_earth_objects'][today_date_string][0]['close_approach_data'][0]['miss_distance']["kilometers"])
+            asteroid_miss_distance_rounded = round(asteroid_miss_distance_raw, 2)
+            asteroid_output.append(f"First asteroid miss distance (km) = {asteroid_miss_distance_rounded}")
+            asteroid_distances.append(round(asteroid_miss_distance_raw, 0))
+        except: return "No asteroids passed near the Earth today."
 
         try:
             second_asteroid_miss_distance_raw = float(data['near_earth_objects'][today_date_string][1]['close_approach_data'][0]['miss_distance']["kilometers"])
             second_asteroid_miss_distance_rounded = round(second_asteroid_miss_distance_raw, 2)
-            print(f"Second asteroid miss distance (km) = {second_asteroid_miss_distance_rounded}")
-            asteroid_distances.append(int(second_asteroid_miss_distance_raw))
-        except:
-            pass
+            asteroid_output.append(f"Second asteroid miss distance (km) = {second_asteroid_miss_distance_rounded}")
+            asteroid_distances.append(round(second_asteroid_miss_distance_raw, 0))
+        except: pass
 
         try:
             third_asteroid_miss_distance_raw = float(data['near_earth_objects'][today_date_string][2]['close_approach_data'][0]['miss_distance']["kilometers"])
             third_asteroid_miss_distance_rounded = round(third_asteroid_miss_distance_raw, 2)
-            print(f"Third asteroid miss distance (km) = {third_asteroid_miss_distance_rounded}")
-            asteroid_distances.append(int(third_asteroid_miss_distance_raw))
-        except:
-            pass
+            asteroid_output.append(f"Third asteroid miss distance (km) = {third_asteroid_miss_distance_rounded}")
+            asteroid_distances.append(round(third_asteroid_miss_distance_raw, 0))
+        except: pass
 
-        self.player_enter_asteroid_distance(asteroid_distances)
+        self.player_enter_asteroid_distance(asteroid_distances, asteroid_output)
 
 
-#the below lines create a class object for the asteroid challenge and run it
-asteroid_challenge = Asteroids("Asteroid Proximity Sensor") #create new class object, adding the name of the challenge
-asteroid_challenge.get_3_asteroid_data()
+# the below lines create a class object for the asteroid challenge and run it
+asteroid_challenge = Asteroids("Asteroid Proximity Sensor")  # create new class object, adding the name of the challenge
+asteroid_challenge.get_all_asteroid_data()
