fb5739c3f537afc0520433aee8b889dfd8b43548
K-1105 <kirstym@me.com>
2023-08-16 18:52:58 +0100

    Update TextInput.py
    Added character length limit based on rectangle size and margins to TextInput.py

--- Scene_files/TextInput.py
+++ Scene_files/TextInput.py
@@ -12,21 +12,25 @@ class TextInput:
         self.color = color
         self.bg_color = bg_color
         self.text = ""
+        self.margins = 5
 
     def handle_event(self, event):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_BACKSPACE:
                 self.text = self.text[:-1]
             else:
-                self.text += event.unicode
+                # Render the new text with the appended character to check its width
+                new_text = self.text + event.unicode
+                txt_surface = self.font.render(new_text, True, self.color)
 
-    # def update(self):
-    #     pass
+                # Check if the width of the rendered text is within the boundary of the rectangle
+                if txt_surface.get_width() <= self.width - self.margins:
+                    self.text = new_text
 
     def draw(self, screen):
         txt_surface = self.font.render(self.text, True, self.color)
         pygame.draw.rect(screen, self.bg_color, (self.x, self.y, self.width, self.height))
-        screen.blit(txt_surface, (self.x, self.y))
+        screen.blit(txt_surface, (self.x + self.margins, self.y))
 
     def user_answer(self):
         return self.text
